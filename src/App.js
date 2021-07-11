6f7dce210adb5cc7a7652f76873346882e3b4560
Resbi-Anik <resbianik13@gmail.com>
2021-07-11 15:11:09 +0600

    Create FOrm modal is completed.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
-// import ReactModal from "./components/modal";
-import Form from "./components/form";
+import ReactModal from "./components/modal";
+
 import ReactCalendar from "./components/reactCalendar";
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 function App() {
-  const handleButtonForm =()=>{
-    
-  }
+  const [modal, setModal] = useState(false);
+  const handleModal = () => {
+    console.log("triggered");
+    setModal(false);
+  };
+  const handleButtonForm = () => {
+    setModal(true);
+  };
   return (
     <div>
-      <Form />
-      <Button variant="success" onClick={handleButtonForm}>Create Form</Button>
-     <ReactCalendar />
+      <Button variant="success" onClick={handleButtonForm}>
+        Create Form
+      </Button>
+      <ReactModal
+        createModalValue={modal}
+        createAppointment
+        createModalShow={handleModal}
+        // user={userDetails}
+      />
+      <ReactCalendar />
     </div>
   );
 }
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Form from "./form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const MyVerticallyCenteredModal = (props) => {
-  console.log('props.users',props.users);
+const ShowUsers = (props) => {
+  console.log("props.users", props.users);
   return (
     <Modal
       {...props}
@@ -36,19 +37,54 @@ const MyVerticallyCenteredModal = (props) => {
   );
 };
 
-const ReactModal = (props) => {
+const CreateUsers = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Create Appointment
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const ReactModal = (props) => {  
   const [modalShow, setModalShow] = React.useState(false);
   const handleModalShow = () => {
     props.modalShow(false);
   };
+
+  const handleCreateModalShow = () => {
+    props.createModalShow(false);
+  };
   return (
     <>
-      {console.log("valueFromProps", props.user)}
-      <MyVerticallyCenteredModal
-        show={props.modalValue}
-        onHide={() => handleModalShow()}
-        users={props.user}
-      />
+      {console.log("valueFromProps", props.createModalValue)}
+      {props.createAppointment ? (
+        <CreateUsers
+          show={props.createModalValue}
+          onHide={() => handleCreateModalShow()}
+          // users={props.user}
+        />
+      ) : (
+        <ShowUsers
+          show={props.modalValue}
+          onHide={() => handleModalShow()}
+          users={props.user}
+        />
+      )}
     </>
   );
 };
